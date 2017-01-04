95e0afc63f8989c2d967f7894a6244653815493f
seulgi_is_love <wheejuni@gmail.com>
2017-01-04 22:53:03 +0900

    -리스트함수 활용(코드잇 예제) _코딧 센세이 지도사항 -while문을 9에서부터 거꾸로 내려옴(b>=0) 원소 하나하나의 홀짝 판정함.

--- listmethod.py
+++ listmethod.py
@@ -17,15 +17,17 @@ print(numbers)
 # 코드를 입력하세요
 
 
-b = 0
-target_range_odd = int(len(numbers)/2)
-print(target_range_odd)
+b = len(numbers)-1
 
-while (b < target_range_odd):
-    print(b)
-    del numbers[b]
-    b += 1
 
+while b >= 0:
+
+    if numbers[b] % 2 == 1:#각 원소 하나하나의 홀/짝 판정
+        del numbers[b]
+
+
+    b = b - 1#거꾸로 내려감
+print(numbers)
 # numbers의 인덱스 0 자리에 20이라는 값 삽입
 # 코드를 입력하세요
 
