a1d4766d16224bcbc506986dd41f8fa556eff4d9
seulgi_is_love <wheejuni@gmail.com>
2017-01-04 20:46:01 +0900

    -환전(코드잇 예제) _ pass.

--- currency.py
+++ currency.py
@@ -1,11 +1,16 @@
 # 원(￦)에서 달러($)로 바꿔주는 함수
 def krw_to_usd(won):
-
+    krw = won
+    usd = krw / 1000
+    return usd
 
 # 코드를 입력하세요.
 
 # 달러($)에서 엔(￥)로 바꿔주는 함수
 def usd_to_jpy(dollars):
+
+    yen = dollars * (1000/8)
+    return yen
 
 
 # 코드를 입력하세요.
@@ -17,11 +22,23 @@ print("한국 화폐: " + str(amounts))
 # amounts를 원(￦)에서 달러($)로 바꿔주기
 # 코드를 입력하세요.
 
+woncount = 0
+while(woncount < len(amounts)):
+    amounts [woncount] = krw_to_usd(amounts[woncount])
+    woncount += 1
+
+
+
 # 달러($)로 각각 얼마인가요?
 print("미국 화폐: " + str(amounts))
 
 # amounts를 달러($)에서 엔(￥)으로 바꿔주기
 # 코드를 입력하세요.
+yencount = 0
+while (yencount < len(amounts)):
+    amounts [yencount] = usd_to_jpy(amounts[yencount])
+    yencount += 1
+
 
 # 엔(￥)으로 각각 얼마인가요?
 print("일본 화폐: " + str(amounts))
