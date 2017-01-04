0e7ab5785b5f6cbb54c12c9e22bc0522603be1d1
seulgi_is_love <wheejuni@gmail.com>
2017-01-04 20:55:04 +0900

    -리스트함수 활용(코드잇 예제) _

---
+++ listmethod.py
@@ -0,0 +1,18 @@
+# 빈 리스트 만들기
+numbers = []
+
+# numbers에 자연수 1부터 10까지 추가
+# 코드를 입력하세요
+print(numbers)
+
+# numbers에서 홀수 제거
+# 코드를 입력하세요
+print(numbers)
+
+# numbers의 인덱스 0 자리에 20이라는 값 삽입
+# 코드를 입력하세요
+print(numbers)
+
+# numbers를 정렬해서 출력
+# 코드를 입력하세요
+print(numbers)
