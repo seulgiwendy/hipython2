72f0fddacad0650456ff994b009ebaf45e5faeed
seulgi_is_love <wheejuni@gmail.com>
2017-01-04 22:42:54 +0900

    -리스트함수 활용(코드잇 예제) _final -아 진짜 힘들었음. 짝/홀 발라내는 부분을 array길이/2로 while문의 타깃을 잡아준 다음에 처리

--- listmethod.py
+++ listmethod.py
@@ -3,16 +3,37 @@ numbers = []
 
 # numbers에 자연수 1부터 10까지 추가
 # 코드를 입력하세요
+a = 1
+app = 1
+while (a <= 10):
+    numbers.append(app)
+    app += 1
+    a +=1
+
+
 print(numbers)
 
 # numbers에서 홀수 제거
 # 코드를 입력하세요
-print(numbers)
+
+
+b = 0
+target_range_odd = int(len(numbers)/2)
+print(target_range_odd)
+
+while (b < target_range_odd):
+    print(b)
+    del numbers[b]
+    b += 1
 
 # numbers의 인덱스 0 자리에 20이라는 값 삽입
 # 코드를 입력하세요
+
+numbers.insert(0, 20)
 print(numbers)
 
 # numbers를 정렬해서 출력
 # 코드를 입력하세요
+
+numbers = sorted(numbers)
 print(numbers)
