3479293811922c2b465a8296c9b542e69eeadb8a
seulgi_is_love <wheejuni@gmail.com>
2017-01-04 19:57:38 +0900

    -온도변환기(코드잇 예제)

---
+++ temperature_convert.py
@@ -0,0 +1,15 @@
+# 화씨 온도에서 섭씨 온도로 바꿔주는 함수
+def fahrenheit_to_celsius(fahrenheit):
+    # 코드를 입력하세요.
+
+# 테스트용 온도 리스트
+sample_temperature_list = [40, 15, 32, 64, -4, 11]
+
+# 화씨 온도 출력
+print("화씨 온도 리스트: " + str(sample_temperature_list))
+
+# 리스트의 값들을 화씨에서 섭씨로 변환
+# 코드를 입력하세요.
+
+# 섭씨 온도 출력
+print("섭씨 온도 리스트: " + str(sample_temperature_list))
